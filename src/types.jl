2b6e31bfa9746a3ed38236b9e1775348045a5157
Rafael <rafaelab@users.noreply.github.com>
2024-11-02 19:09:00 +0100

    initial commit

---
+++ src/types.jl
@@ -0,0 +1,49 @@
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This module provides the types used in the `BibLibrary` module.
+These are the general BibTeX types useful for dispatching entries on them.
+"""
+abstract type BibType end
+
+# ----------------------------------------------------------------------------------------------- #
+#
+struct Article <: BibType end
+struct Book <: BibType end
+struct Booklet <: BibType end
+struct Conference <: BibType end
+struct InBook <: BibType end
+struct InCollection <: BibType end
+struct InProceedings <: BibType end
+struct Manual <: BibType end
+struct MasterThesis <: BibType end
+struct Misc <: BibType end
+struct PhdThesis <: BibType end
+struct Proceedings <: BibType end
+struct TechReport <: BibType end
+struct Unpublished <: BibType end
+
+
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+Returns the name of the type of the entry as a string.
+"""
+getTypeName(::Type{Article}) = "article"
+getTypeName(::Type{Book}) = "book"
+getTypeName(::Type{Booklet}) = "booklet"
+getTypeName(::Type{Conference}) = "conference"
+getTypeName(::Type{InBook}) = "inbook"
+getTypeName(::Type{InCollection}) = "incollection"
+getTypeName(::Type{InProceedings}) = "inproceedings"
+getTypeName(::Type{Manual}) = "manual"
+getTypeName(::Type{MasterThesis}) = "mastersthesis"
+getTypeName(::Type{Misc}) = "misc"
+getTypeName(::Type{PhdThesis}) = "phdthesis"
+getTypeName(::Type{Proceedings}) = "proceedings"
+getTypeName(::Type{TechReport}) = "techreport"
+getTypeName(::Type{Unpublished}) = "unpublished"
+
+
+# ----------------------------------------------------------------------------------------------- #
+#
