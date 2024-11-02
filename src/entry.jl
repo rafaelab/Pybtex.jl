8f0452c8989478b15cd86fd1af96bff2b9d3306b
Rafael <rafaelab@users.noreply.github.com>
2024-11-02 19:12:00 +0100

    add bibtex entry types to optimise dispatch

--- src/entry.jl
+++ src/entry.jl
@@ -1,18 +1,45 @@
-struct BibEntry
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This object represents a BibTeX entry. 
+It is a wrapper around pybtex's `pybtex.database.Entry` object, connecting it with its corresponding key.
+"""
+struct BibEntry{T <: BibType}
 	key::String
 	info::Py
 end
 
-
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function checks if a field is present in a BibTeX entry.
+"""
 hasField(entry::BibEntry, field::String) = haskey(pyconvert(Dict, entry.info.fields), lowercase(field))
 
+
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns a BibTeX entry from a BibTeX library.
+"""
 getEntry(bib::BibLibrary, key) = BibEntry(key, bib.entries[key])
 
-function getType(entry::BibEntry) 
-	s = stringPy2Jl(entry.info.type)
-	return lowercase(s)
+
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the type of a BibTeX entry.
+"""
+function getType(::BibEntry{T}) where {T} 
+	return getTypeName(T)
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the authors of a BibTeX entry.
+The authors are returned as a vector of `PersonName` objects.
+"""
 function getAuthors(entry::BibEntry)
 	authors = entry.info.persons["author"]
 	names = []
@@ -25,6 +52,12 @@ function getAuthors(entry::BibEntry)
 	return names
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the editors of a BibTeX entry.
+The editors are returned as a vector of `PersonName` objects.
+"""
 function getEditors(entry::BibEntry)
 	authors = entry.info.persons["editors"]
 	names = []
@@ -37,6 +70,8 @@ function getEditors(entry::BibEntry)
 	return names
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
 function getTitle(entry::BibEntry)
 	if ! hasField(entry, "title")
 		@warn "No title field in entry \"$(entry.key)\"."
@@ -48,6 +83,11 @@ function getTitle(entry::BibEntry)
 	return fixStrings(s)
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the year of the publication.
+"""
 function getYear(entry::BibEntry)
 	if ! hasField(entry, "year")
 		@warn "No year field in entry \"$(entry.key)\"."
@@ -57,6 +97,11 @@ function getYear(entry::BibEntry)
 	return stringPy2Jl(entry.info.fields["year"])
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the journal where the publication was published.
+"""
 function getJournal(entry::BibEntry)
 	if ! hasField(entry, "journal")
 		@warn "No journal field in entry \"$(entry.key)\"."
@@ -66,47 +111,106 @@ function getJournal(entry::BibEntry)
 	return stringPy2Jl(entry.info.fields["journal"])
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the volume of the publication.
+"""
 function getVolume(entry::BibEntry)
 	return hasField(entry, "volume") ? stringPy2Jl(entry.info.fields["volume"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the number of the publication.
+"""
 function getNumber(entry::BibEntry)
 	return hasField(entry, "number") ? stringPy2Jl(entry.info.fields["number"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the pages of the publication.
+"""
 function getPages(entry::BibEntry)
 	return hasField(entry, "pages") ? stringPy2Jl(entry.info.fields["pages"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the edition of the publication.
+"""
 function getEdition(entry::BibEntry)
 	return hasField(entry, "edition") ? stringPy2Jl(entry.info.fields["edition"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the location of the publication.
+"""
 function getLocation(entry::BibEntry)
 	return hasField(entry, "location") ? stringPy2Jl(entry.info.fields["location"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the publisher of the publication.
+"""
 function getPublisher(entry::BibEntry)
 	return hasField(entry, "publisher") ? stringPy2Jl(entry.info.fields["publisher"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the DOI of the publication.
+"""
 function getDOI(entry::BibEntry)
 	return hasField(entry, "doi") ? stringPy2Jl(entry.info.fields["doi"]) : ""
 end
 
+
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the URL of the publication.
+"""
 function getURL(entry::BibEntry)
 	return hasField(entry, "url") ? stringPy2Jl(entry.info.fields["url"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the abstract of the publication, if available.
+"""
 function getAbstract(entry::BibEntry)
 	return  hasField(entry, "abstract") ? string(entry.info.fields["abstract"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the keywords of the publication, if available.
+"""
 function getKeywords(entry::BibEntry)
 	return  hasField(entry, "keywords") ? string(entry.info.fields["keywords"]) : ""
 end
 
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+This function returns the BibTeX representation of the entry.
+"""
 function getBibTeX(entry::BibEntry)
 	return string(entry.info.to_string("bibtex"))
 end
 
+
+# ----------------------------------------------------------------------------------------------- #
+#
