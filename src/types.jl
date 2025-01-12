b90e64aa246d98ed204bb6e6bffd6e9e3f196158
Rafael <rafaelab@users.noreply.github.com>
2025-01-12 18:07:20 +0100

    whitespace

--- src/types.jl
+++ src/types.jl
@@ -24,7 +24,6 @@ struct TechReport <: BibType end
 struct Thesis <: BibType end
 struct Unpublished <: BibType end
 
-
 # ----------------------------------------------------------------------------------------------- #
 #
 typeDict = Dict(
