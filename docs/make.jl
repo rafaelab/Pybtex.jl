853b38b5c0501773362e125fa7463a1750ad0106
Rafael <rafaelab@users.noreply.github.com>
2026-03-01 13:57:01 +0100

    update dependencies; fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,16 +6,15 @@ DocMeta.setdocmeta!(Pybtex, :DocTestSetup, :(using Pybtex); recursive = true)
 makedocs(
     sitename = "Pybtex.jl",
     modules = [Pybtex],
+    format   = Documenter.HTML(
+		prettyurls = get(ENV, "CI", nothing) == "true",
+	),
     pages = [
         "Home" => "index.md",
     ],
 )
 
-repo_env = get(ENV, "GITHUB_REPOSITORY", "")
-if !isempty(repo_env)
-    deploydocs(
-        repo = "github.com/$(repo_env).git",
-        devbranch = get(ENV, "DOCUMENTER_DEVBRANCH", "main"),
-        push_preview = true,
-    )
-end
+deploydocs(
+	repo   = "github.com/rafaelab/Pybtex.jl.git",
+	devbranch = "main",
+)
